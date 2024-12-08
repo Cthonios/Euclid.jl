42b312b6de243d67ed6eb43426df540141a2793e
Documenter.jl <documenter@juliadocs.github.io>
2024-12-08 21:27:45 +0000

    build based on 11adfd1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Euclid","category":"page"},{"location":"#Euclid","page":"Home","title":"Euclid","text":"","category":"section"}]
+}
